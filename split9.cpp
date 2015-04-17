52835bd5c93d0b8bb0f42727ea88db1342ef57cd
Dave Johansen <davejohansen@gmail.com>
2015-04-16 21:41:02 -0700

    Add version that uses boost::algorithm::split()

---
+++ split9.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <vector>
+
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/algorithm/string/split.hpp>
+
+
+int main()
+{
+   std::string input_line;
+   long count = 0;
+   int sec, lps;
+   time_t start = time(NULL);
+
+   std::cin.sync_with_stdio(false); //disable synchronous IO
+
+   std::vector<std::string> spline;
+   std::size_t numWords = 0;
+   std::size_t numChars = 0;
+
+   while(std::cin)
+   {
+      std::getline(std::cin, input_line);
+      boost::algorithm::split(spline, input_line, boost::algorithm::is_any_of(" "));
+      numWords += spline.size();
+      for (std::vector<std::string>::const_iterator iter = spline.begin(); iter != spline.end(); ++iter)
+         numChars += iter->size();
+      count++;
+   };
+
+   count--; //subtract for final over-read
+   sec = (int) time(NULL) - start;
+   std::cerr << "C++   : Saw " << count << " lines (" << numWords << " words/" << numChars << " chars) in " << sec << " seconds." ;
+
+   if (sec > 0)
+   {
+      lps = count / sec;
+      std::cerr << "  Crunch speed: " << lps << std::endl;
+   }
+   else
+      std::cerr << std::endl;
+
+   return 0;
+}
